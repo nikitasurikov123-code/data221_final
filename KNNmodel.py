78410082964c32dd428274ca2f00bf841c1fb6c7
nikitasurikov123-code <nikitasurikov123@gmail.com>
2026-03-24 22:09:01 -0600

    Update KNNmodel.py
    added the needed libraries

---
+++ KNNmodel.py
@@ -0,0 +1,7 @@
+import pandas
+import numpy
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error
