269be45dcee71fa9808ab567c5cfc7524b3bf6b4
nikitasurikov123-code <nikitasurikov123@gmail.com>
2026-03-26 00:01:24 -0600

    made the df and removed column sthat aren't needed/formtatted terribly

--- KNNmodel.py
+++ KNNmodel.py
@@ -1,7 +1,15 @@
 import pandas
 import numpy
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_absolute_error
+
+data_frame = pandas.read_csv("car details v4.csv")
+
+rupee_to_cad = 0.01525
+data_frame["Price"] = data_frame["Price"] * rupee_to_cad
+
+data_frame = data_frame.drop(columns = ["Color", "Length", "Width", "Height", "Max Power", "Max Torque"])
+
+cleaned_engine = []
